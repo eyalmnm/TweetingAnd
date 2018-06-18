02d916cdf7ea4d134b334f460519ddf2b25d0562
eyalmnm <eyal@em-projects.com>
2018-06-18 23:43:41 +0300

    "first commit"

--- app/src/main/java/com/em_projects/tweetings/utils/StringUtils.kt
+++ app/src/main/java/com/em_projects/tweetings/utils/StringUtils.kt
@@ -3,6 +3,9 @@ package com.em_projects.tweetings.utils
 class StringUtils {
 
     companion object {
+        /**
+         * This method check whether the given String is null or empty (after removing some characters)
+         */
         fun isNullOrEmpty(str: String?): Boolean {
             if (str == null) return true
             if (str.isEmpty()) return true

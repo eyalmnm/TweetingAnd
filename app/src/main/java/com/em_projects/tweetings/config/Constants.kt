cf8ef1521ee327ec7b06c618f844183f5b92384e
eyalmnm <eyal@em-projects.com>
2018-07-07 11:54:05 +0300

    Add Server URL reference

--- app/src/main/java/com/em_projects/tweetings/config/Constants.kt
+++ app/src/main/java/com/em_projects/tweetings/config/Constants.kt
@@ -1,7 +1,11 @@
 package com.em_projects.tweetings.config
 
+import com.em_projects.tweetings.BuildConfig
+
 class Constants {
     companion object {
+
+        val TWEET_SERVER_URL = BuildConfig.SERVER_URL
 
         // General Actions
         val ACTION_OPERATION_CANCELLED = "action_operation_cancelled"
